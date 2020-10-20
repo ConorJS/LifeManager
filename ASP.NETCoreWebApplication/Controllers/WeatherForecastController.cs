090ea5a7ee379e5b31d1af166e9b8818caf3e5ff
Conor <aeimo@protonmail.com>
2020-10-20 23:06:25 +1100

    Minor formatting.

--- ASP.NETCoreWebApplication/Controllers/WeatherForecastController.cs
+++ ASP.NETCoreWebApplication/Controllers/WeatherForecastController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,7 +8,7 @@ namespace ASP.NETCoreWebApplication.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase {
-        private static readonly string[] Summaries = new[] {
+        private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
@@ -22,7 +21,9 @@ namespace ASP.NETCoreWebApplication.Controllers {
         [HttpGet]
         public IEnumerable<WeatherForecast> Get() {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+            return Enumerable
+                .Range(1, 5)
+                .Select(index => new WeatherForecast {
                     Date = DateTime.Now.AddDays(index),
                     TemperatureC = rng.Next(-20, 55),
                     Summary = Summaries[rng.Next(Summaries.Length)]
