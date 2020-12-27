e8a48a864fc419123dbb38ec91c6bcec2085d790
Conor <aeimo@protonmail.com>
2020-12-27 21:17:27 +1100

    More sensible state management for To Do creation/editing.

--- LifeManager/ClientApp/src/components/to-do-task-viewer.tsx
+++ LifeManager/ClientApp/src/components/to-do-task-viewer.tsx
@@ -5,15 +5,9 @@ export const ToDoTaskViewer: FunctionComponent = () => {
 
     const [doRefresh, setDoRefresh] = useState<boolean>(true);
     const [toDoTasks, setToDoTasks] = useState<ToDoTask[]>([]);
-
-    const [creatingItem, setCreatingItem] = useState<boolean>(false);
-    const [creatingItemName, setCreatingItemName] = useState<string>('');
-    const [creatingItemRelativeSize, setCreatingItemRelativeSize] = useState<number>(1);
-
-    const [editingItem, setEditingItem] = useState<ToDoTask>();
-    const [editingItemName, setEditingItemName] = useState<string>('');
-    const [editingItemRelativeSize, setEditingItemRelativeSize] = 
-        useState<number>(() => !editingItem || !editingItem.relativeSize ? 1 : editingItem.relativeSize);
+    const [activeAction, setActiveAction] = useState<Action>(Action.NONE);
+    const [activeItemDetails, setActiveItemDetails] = useState<ActiveItemDetails>(new ActiveItemDetails());
+    const [itemBeingEdited, setItemBeingEdited] = useState<ToDoTask>();
 
     //== methods ======================================================================================================
 
@@ -22,58 +16,65 @@ export const ToDoTaskViewer: FunctionComponent = () => {
         return await response.json();
     }
 
-    const displayCreateToDoTaskUi = (): void => {
-        setCreatingItem(true);
+    const changeAction = (activeItemType: Action): void => {
+        setActiveItemDetails({...activeItemDetails,
+            newName: '',
+            newRelativeSize: 1
+        });
         
-        closeEditToDoTaskUi();
+        setActiveAction(activeItemType);
+    }
+    
+    const stopAction = (): void => {
+        changeAction(Action.NONE);
+        setItemBeingEdited(undefined);
+    }
+    
+    function creating(): boolean { 
+        return Action.CREATE === activeAction; 
+    }
+    
+    function editing(): boolean { 
+        return Action.EDIT === activeAction; 
     }
 
-    const displayEditToDoTaskUi = (toDoTask: ToDoTask): void => {
-        setEditingItem(toDoTask);
-        setEditingItemName(toDoTask.name);
-        setEditingItemRelativeSize(toDoTask.relativeSize);
-
-        closeCreateToDoTaskUi();
-    }
-
-    const closeCreateToDoTaskUi = (): void => {
-        setCreatingItem(false);
-    }
-
-    const closeEditToDoTaskUi = (): void => {
-        setEditingItem(undefined);
-        setEditingItemName('');
-        setEditingItemRelativeSize(1);
-    }
-
-    function creatingItemNameChangeHandler(changeEvent: SyntheticEvent): void {
-        const target = changeEvent.target as HTMLInputElement;
-        setCreatingItemName(target.value);
-    }
-
-    function creatingItemRelativeSizeChangeHandler(changeEvent: SyntheticEvent): void {
-        const target = changeEvent.target as HTMLInputElement;
-        setCreatingItemRelativeSize(Number.parseInt(target.value));
-    }
-
-    function editingItemNameChangeHandler(changeEvent: SyntheticEvent): void {
-        const target = changeEvent.target as HTMLInputElement;
-        setEditingItemName(target.value);
-    }
-
-    function editingItemRelativeSizeChangeHandler(changeEvent: SyntheticEvent): void {
-        const target = changeEvent.target as HTMLInputElement;
-        setEditingItemRelativeSize(Number.parseInt(target.value));
-    }
-
-    const createToDoTask = () => {
-        if (!creatingItemName || !creatingItemRelativeSize) {
-            // TODO: This problem should be addressed with form validation, and appropriate error messages.
-            console.log("Required fields missing; cannot create a To Do task.");
+    function activeItemAttributeChangeHandler(changeEvent: SyntheticEvent, itemAttribute: ItemAttribute): void {
+        if (Action.NONE === activeAction) {
+            console.error(`Tried to update value ${itemAttribute}, but there is no active item.`);
             return;
         }
 
-        const toDoTask = new ToDoTask(creatingItemName, creatingItemRelativeSize);
+        const target: HTMLInputElement = changeEvent.target as HTMLInputElement;
+        const value: any = target.value;
+
+        switch (itemAttribute) {
+            case ItemAttribute.NAME:
+                setActiveItemDetails({...activeItemDetails, newName: value as string});
+                break;
+
+            case ItemAttribute.RELATIVE_SIZE:
+                setActiveItemDetails({...activeItemDetails, newRelativeSize: Number.parseInt(value)});
+                break;
+        }
+    }
+
+    const editItem = (toDoTask: ToDoTask) => {
+        changeAction(Action.EDIT); 
+        setActiveItemDetails({...activeItemDetails,
+            newName: toDoTask.name,
+            newRelativeSize: toDoTask.relativeSize
+        })
+        setItemBeingEdited(toDoTask)
+    }
+    
+    const createToDoTask = () => {
+        if (!creating()) {
+            // TODO: This problem should be addressed with form validation, and appropriate error messages.
+            console.log("We are not in edit mode, yet saveToDoTask was called.");
+            return;
+        }
+
+        const toDoTask = new ToDoTask(activeItemDetails.newName, activeItemDetails.newRelativeSize);
 
         const requestOptions = {
             method: 'POST',
@@ -85,32 +86,32 @@ export const ToDoTaskViewer: FunctionComponent = () => {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                closeCreateToDoTaskUi();
+                stopAction();
                 refresh();
             });
     }
 
     const saveToDoTask = (): void => {
-        if (!editingItem || !editingItemName || !editingItemRelativeSize) {
+        if (!editing() || !itemBeingEdited) {
             // TODO: This problem should be addressed with form validation, and appropriate error messages.
-            console.log("Required fields missing; cannot update To Do task.");
+            console.log(`${editing() ? "No item is being edited" : "We are not in edit mode"}, yet saveToDoTask was called.`);
             return;
         }
-
-        editingItem.name = editingItemName;
-        editingItem.relativeSize = editingItemRelativeSize;
 
         const requestOptions = {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(editingItem)
+            body: JSON.stringify({...itemBeingEdited,
+                name: activeItemDetails.newName,
+                relativeSize: activeItemDetails.newRelativeSize
+            })
         };
 
         fetch('api/ToDoTask/Update', requestOptions)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setEditingItem(undefined);
+                stopAction();
                 refresh();
             });
     }
@@ -118,8 +119,8 @@ export const ToDoTaskViewer: FunctionComponent = () => {
     const removeToDoTask = (toDoTask: ToDoTask): void => {
         fetch(`api/ToDoTask/Remove/${toDoTask.id}`).then(() => {
             // Close the edit window if the item being edited was just removed.
-            if (toDoTask === editingItem) {
-                closeEditToDoTaskUi();
+            if (itemBeingEdited === toDoTask) {
+                stopAction();
             }
             
             refresh();
@@ -134,6 +135,7 @@ export const ToDoTaskViewer: FunctionComponent = () => {
 
     //== execution ====================================================================================================
 
+    // TODO: Refactor this away?
     if (doRefresh) {
         refresh();
         setDoRefresh(false);
@@ -145,10 +147,10 @@ export const ToDoTaskViewer: FunctionComponent = () => {
     const items: JSX.Element[] = [];
     toDoTasks.forEach((toDoTask, index) => {
         items.push(
-            <div key={'toDoTask' + index}
-                 onClick={() => displayEditToDoTaskUi(toDoTask)}>
-                {toDoTask.name} ({toDoTask.relativeSize} at {toDoTask.dateTimeCreated})
-
+            <div key={'toDoTask' + index}>
+                <div onClick={() => editItem(toDoTask)}>
+                    {toDoTask.name} ({toDoTask.relativeSize} at {toDoTask.dateTimeCreated})
+                </div>
                 <button onClick={() => removeToDoTask(toDoTask)}>
                     R
                 </button>
@@ -162,71 +164,37 @@ export const ToDoTaskViewer: FunctionComponent = () => {
             <div>{items}</div>
 
             <button className="btn btn-primary"
-                    onClick={displayCreateToDoTaskUi}>
+                    onClick={() => changeAction(Action.CREATE)}>
                 New
             </button>
 
-            <div className="create-details"
-                 style={creatingItem ? {} : displayNone}>
-
-                <div>Creating a To Do task...</div>
+            <div style={editing() || creating() ? {} : displayNone}>
+                <div>{editing() ? "Edit" : "Creat"}ing a To Do task...</div>
 
                 <div>
-                    <label htmlFor="creating-todo-task-name">Name</label>
-                    <input id="creating-todo-task-name"
+                    <label htmlFor="active-todo-task-name">Name</label>
+                    <input id="active-todo-task-name"
                            type="string"
-                           value={creatingItemName}
-                           onChange={creatingItemNameChangeHandler}/>
-                </div>
-
-                <div>
-                    <label htmlFor="creating-todo-task-relative-size">Relative Size</label>
-                    <input id="creating-todo-task-relative-size"
-                           type="number"
-                           value={creatingItemRelativeSize}
-                           onChange={creatingItemRelativeSizeChangeHandler}/>
-                </div>
-
-                <div>
-                    <button className="btn btn-primary"
-                            onClick={createToDoTask}>
-                        Create
-                    </button>
-
-                    <button className="btn btn-primary"
-                            onClick={closeCreateToDoTaskUi}>
-                        Cancel
-                    </button>
-                </div>
-            </div>
-
-            <div style={!!editingItem ? {} : displayNone}>
-                <div>Editing a To Do task...</div>
-
-                <div>
-                    <label htmlFor="editing-todo-task-name">Name</label>
-                    <input id="editing-todo-task-name"
-                           type="string"
-                           value={editingItemName}
-                           onChange={editingItemNameChangeHandler}/>
+                           value={activeItemDetails.newName}
+                           onChange={(event) => activeItemAttributeChangeHandler(event, ItemAttribute.NAME)}/>
                 </div>
 
                 <div>
                     <label htmlFor="editing-todo-task-relative-size">Relative Size</label>
                     <input id="editing-todo-task-relative-size"
                            type="number"
-                           value={editingItemRelativeSize}
-                           onChange={editingItemRelativeSizeChangeHandler}/>
+                           value={activeItemDetails.newRelativeSize}
+                           onChange={(event) => activeItemAttributeChangeHandler(event, ItemAttribute.RELATIVE_SIZE)}/>
                 </div>
 
                 <div>
                     <button className="btn btn-primary"
-                            onClick={saveToDoTask}>
+                            onClick={editing() ? saveToDoTask : createToDoTask}>
                         Save
                     </button>
 
                     <button className="btn btn-primary"
-                            onClick={closeEditToDoTaskUi}>
+                            onClick={stopAction}>
                         Cancel
                     </button>
                 </div>
@@ -236,6 +204,12 @@ export const ToDoTaskViewer: FunctionComponent = () => {
 }
 
 //== types ============================================================================================================
+
+export class ActiveItemDetails {    
+    newName: string = '';
+    
+    newRelativeSize: number = 1;
+}
 
 export class ToDoTask {
     id?: Number;
@@ -252,4 +226,15 @@ export class ToDoTask {
         this.name = name;
         this.relativeSize = relativeSize;
     }
+}
+
+export enum Action {
+    NONE,
+    CREATE,
+    EDIT
+}
+
+export enum ItemAttribute {
+    NAME,
+    RELATIVE_SIZE
 }
