e379d4d7e57d09e2be61987462ecd304b488015c
Conor <aeimo@protonmail.com>
2021-02-07 19:53:17 +1100

    Fixed bug when running Flyway migrations on Linux.

--- LifeManager/Environment/Database/MigrateDatabase.sh
+++ LifeManager/Environment/Database/MigrateDatabase.sh
@@ -36,15 +36,11 @@ cd ../..
 download_and_extract_to "$FLYWAY_URL" "$FLYWAY_INSTALLATION_DIR" "Flyway" "$FLYWAY_HASH"
 
 # Migrate PostgreSQL database
-if platform_is_linux; then
-	if [ ! -x "/usr/local/bin/flyway" ]; then
-		sudo ln -s "$(pwd)"'/'"$flyway_directory""/flyway" "/usr/local/bin"
-	fi
-	
+if platform_is_linux; then	
 	# Workaround for Flyway 7.3.1 to make JRE bundled with Flyway executable (otherwise the script fails in a cryptic manner)
 	bundled_jre_executable="$FLYWAY_INSTALLATION_DIR""/flyway-""$FLYWAY_VERSION_STRING""/jre/bin/java"
 	sudo chmod 774 $bundled_jre_executable
-	flyway -configFiles="$flyway_config_file" migrate
+	execute_script_hardened "$(pwd)"'/'"$flyway_directory""/flyway" -configFiles="$flyway_config_file" migrate
 	sudo chmod 554 $bundled_jre_executable
 	
 elif platform_is_windows; then
